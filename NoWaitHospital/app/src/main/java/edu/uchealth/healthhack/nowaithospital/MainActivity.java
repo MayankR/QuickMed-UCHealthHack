38749259c98d455218bd123f48f2db5c8f7dfae0
Mayank <rajoria_m@yahoo.com>
2018-10-06 20:44:47 -0700

    User sign up

--- NoWaitHospital/app/src/main/java/edu/uchealth/healthhack/nowaithospital/MainActivity.java
+++ NoWaitHospital/app/src/main/java/edu/uchealth/healthhack/nowaithospital/MainActivity.java
@@ -1,31 +1,105 @@
 package edu.uchealth.healthhack.nowaithospital;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button proceedButton;
+    List<List<String>> patients;
+    LinearLayout patientLL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        Utility.setupDB(this);
 
         proceedButton = (Button) findViewById(R.id.proceed_button);
         proceedButton.setOnClickListener(this);
+
+        patientLL = (LinearLayout) findViewById(R.id.patient_d_ll);
+
+        patients = Utility.getPeople();
+        Log.d("MA", patients.size() + " number of people!");
+        if(patients.size() == 0) {
+            goToSignUp();
+        }
+        displayPatients();
+    }
+
+    void displayPatients() {
+        Resources r = getResources();
+        int px35 = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                35,
+                r.getDisplayMetrics()
+        );
+        int px10 = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                10,
+                r.getDisplayMetrics()
+        );
+        int px15 = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                15,
+                r.getDisplayMetrics()
+        );
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(0, px10, 0, 0);
+
+        patientLL.removeAllViews();
+        for(int i=0;i<patients.size();i++) {
+            final int patientID = i;
+            View child = getLayoutInflater().inflate(R.layout.patient_detail, null);
+            ((TextView)child.findViewById(R.id.pat_name_tv)).setText(patients.get(i).get(0));
+            ((TextView)child.findViewById(R.id.pat_age_g_tv)).setText(patients.get(i).get(2) + ", " + patients.get(i).get(1) + " years");
+
+
+            child.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    selectedPatient(patientID);
+                }
+            });
+            patientLL.addView(child, layoutParams);
+        }
+
+    }
+
+    void selectedPatient(int id) {
+        Intent it = new Intent(this, QuestionsActivity.class);
+        PatientData.name = patients.get(id).get(0);
+        PatientData.age = patients.get(id).get(1);
+        PatientData.gender = patients.get(id).get(2);
+        startActivity(it);
+        finish();
+    }
+
+    void goToSignUp() {
+        Intent it = new Intent(this, UserDataActivity.class);
+        startActivity(it);
+        finish();
     }
 
     @Override
     public void onClick(View view) {
         switch(view.getId()) {
             case R.id.proceed_button:
-                Intent it = new Intent(this, QuestionsActivity.class);
+                Intent it = new Intent(this, UserDataActivity.class);
                 startActivity(it);
         }
     }
--- NoWaitHospital/app/src/main/java/edu/uchealth/healthhack/nowaithospital/QuestionsActivity.java
+++ NoWaitHospital/app/src/main/java/edu/uchealth/healthhack/nowaithospital/QuestionsActivity.java
@@ -3,11 +3,14 @@ package edu.uchealth.healthhack.nowaithospital;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
+import android.speech.RecognizerIntent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -15,6 +18,7 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,11 +28,14 @@ public class QuestionsActivity extends AppCompatActivity implements View.OnClick
     Question curQuestion;
     List<Question> allQuestions;
     int curQuestionIndex;
-    FloatingActionButton nine11FAB, nextFAB;
-    TextView questionTV;
+    FloatingActionButton nine11FAB, nextFAB, speechFAB;
+    TextView questionTV, patientNameTV;
     RelativeLayout questionRL;
     LinearLayout answerLL;
     EditText answerET;
+    float x1, x2;
+    final int SWIPE_THRESHOLD = 300;
+    private final int REQUEST_SPEECH_RECOGNIZER = 3000;
 
 
     @Override
@@ -46,9 +53,13 @@ public class QuestionsActivity extends AppCompatActivity implements View.OnClick
         nine11FAB.setOnClickListener(this);
         nextFAB = (FloatingActionButton) findViewById(R.id.next_fab);
         nextFAB.setOnClickListener(this);
+        speechFAB = (FloatingActionButton) findViewById(R.id.speech_fab);
+        speechFAB.setOnClickListener(this);
 
         questionTV = (TextView) findViewById(R.id.question_tv);
         questionTV.setOnClickListener(this);
+        patientNameTV = (TextView) findViewById(R.id.user_name_tv);
+        patientNameTV.setText(PatientData.name);
 
         answerLL = (LinearLayout) findViewById(R.id.answer_ll);
 
@@ -66,6 +77,21 @@ public class QuestionsActivity extends AppCompatActivity implements View.OnClick
         if(curQuestion.getClass().getSimpleName().equals("MCQQuestion")) {
             List<String> answersList = ((MCQQuestion)curQuestion).getAnswers();
 
+            Resources r = getResources();
+            int px35 = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    35,
+                    r.getDisplayMetrics()
+            );
+            int px10 = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    10,
+                    r.getDisplayMetrics()
+            );
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(px35, px10, px35, 0);
+
             answerLL.removeAllViews();
             for(int i=0;i<answersList.size();i++) {
                 final int optionID = i;
@@ -77,7 +103,7 @@ public class QuestionsActivity extends AppCompatActivity implements View.OnClick
                         clickedOption(optionID);
                     }
                 });
-                answerLL.addView(child);
+                answerLL.addView(child, layoutParams);
             }
 
             answerET = null;
@@ -111,8 +137,57 @@ public class QuestionsActivity extends AppCompatActivity implements View.OnClick
         speakOutQuestion();
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        int action = MotionEventCompat.getActionMasked(event);
+
+        switch(action) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float deltaX = x2 - x1;
+                if (deltaX > SWIPE_THRESHOLD) {
+                    goToPrevQuestion();
+                }
+                break;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(curQuestionIndex == 0) {
+            Intent it = new Intent(this, MainActivity.class);
+            startActivity(it);
+            finish();
+        }
+        goToPrevQuestion();
+    }
+
+    void goToPrevQuestion() {
+        if(curQuestionIndex == 0) {
+            return;
+        }
+        curQuestionIndex--;
+        curQuestion = allQuestions.get(curQuestionIndex);
+        for(int i=allQuestions.size()-1;i>curQuestionIndex;i--) {
+            allQuestions.remove(i);
+        }
+        renderScreen();
+    }
+
     void goToNextQuestion() {
         curQuestionIndex++;
+        Log.d("QA", curQuestionIndex + " " + allQuestions.size());
+        if(curQuestionIndex == allQuestions.size()) {
+            Intent it = new Intent(this, DoneQuestionsActivity.class);
+            startActivity(it);
+            finish();
+            return;
+        }
         curQuestion = allQuestions.get(curQuestionIndex);
         renderScreen();
     }
@@ -125,6 +200,7 @@ public class QuestionsActivity extends AppCompatActivity implements View.OnClick
         }
     }
 
+    // TODO
     void speakOutQuestion() {
 
     }
@@ -132,6 +208,40 @@ public class QuestionsActivity extends AppCompatActivity implements View.OnClick
     void registerResponse() {
         allQuestions.addAll(curQuestion.getNextQuestions(answerET.getText().toString().trim()));
         goToNextQuestion();
+    }
+
+    void registerResponse(String s) {
+        if(curQuestion.getClass().getSimpleName().equals("TextQuestion")) {
+            answerET.setText(s);
+        }
+        allQuestions.addAll(curQuestion.getNextQuestions(s));
+        goToNextQuestion();
+    }
+
+    private void startSpeechRecognizer() {
+        Intent intent = new Intent
+                (RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, curQuestion.getQuestion());
+        startActivityForResult(intent, REQUEST_SPEECH_RECOGNIZER);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_SPEECH_RECOGNIZER) {
+            if (resultCode == RESULT_OK) {
+                List<String> results = data.getStringArrayListExtra
+                        (RecognizerIntent.EXTRA_RESULTS);
+                String mAnswer = results.get(0);
+
+                Toast.makeText(this, mAnswer, Toast.LENGTH_SHORT).show();
+                registerResponse(mAnswer);
+            }
+        }
     }
 
     @Override
@@ -147,6 +257,10 @@ public class QuestionsActivity extends AppCompatActivity implements View.OnClick
                 break;
             case R.id.next_fab:
                 registerResponse();
+                break;
+            case R.id.speech_fab:
+                startSpeechRecognizer();
+                break;
         }
     }
 }
